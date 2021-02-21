22db533af082dd4daa8921930067935255590792
ClayDeveloper <developer@claysucht.eu>
2021-02-21 17:21:25 +0100

    Added JoinHandler class

--- src/dev/claysucht/soupffa/Main.java
+++ src/dev/claysucht/soupffa/Main.java
@@ -4,9 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    private static Main instance;
     @Override
     public void onEnable() {
 
-        
+        instance = this;
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 }
