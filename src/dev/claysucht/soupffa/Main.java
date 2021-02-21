1499efe59c0acdfd8422049b302fc4de1f661ed0
ClayDeveloper <developer@claysucht.eu>
2021-02-21 16:58:34 +0100

    Updated Main class

---
+++ src/dev/claysucht/soupffa/Main.java
@@ -0,0 +1,12 @@
+package dev.claysucht.soupffa;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        
+    }
+}
